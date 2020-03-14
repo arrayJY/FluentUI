8026aba8f97a65cd4a166a2e027fb96c3dea07a3
arrayJY <ji957455952@163.com>
2020-03-15 02:28:10 +0800

    ✨Add basic application class.

---
+++ FluentUI/Application.cpp
@@ -0,0 +1,15 @@
+#include "Application.h"
+
+void skiaui::Application::initGLFW()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_STENCIL_BITS, 0);
+	glfwWindowHint(GLFW_DEPTH_BITS, 0);
+}
+
+void skiaui::Application::exec()
+{ }
---
+++ FluentUI/Application.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <list>
+#include "../3rdparty/glfw/include/GLFW/glfw3.h"
+
+namespace skiaui
+{
+	class Application
+	{
+	public:
+		static Application& instance()
+		{ 
+			static Application __instance;
+			return __instance; 
+		}
+		void exec();
+	private:
+		//Singleton pattern, disable constructors.
+		Application()
+		{
+			initGLFW();
+		}
+		Application(const Application&);
+
+		void initGLFW();
+	};
+}
+
